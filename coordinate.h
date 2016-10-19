ca0c2e99d740f75a76f9c83a6f83c8d5067919e2
Ignatius <ice.emissary@gmail.com>
2016-10-19 08:10:25 +0800

    加入向量基本計算的功能宣告，但實體尚未實做。

--- coordinate.h
+++ coordinate.h
@@ -5,9 +5,9 @@
 #define _COORDINATE_H_
 
 /**
- * Point.
+ * Vector.
  */
-class TCoordinate
+class TVector
 {
 public:
     double x;
@@ -15,15 +15,38 @@ public:
     double z;
 
 public:
-    TCoordinate() : x(0), y(0), z(0) {}
-    TCoordinate(double x, double y, double z) : x(x), y(y), z(z) {}
-    TCoordinate(const TCoordinate &src) : x(src.x), y(src.y), z(src.z) {}
+    TVector() : x(0), y(0), z(0) {}
+    TVector(double x, double y, double z) : x(x), y(y), z(z) {}
+    TVector(const TVector &src) : x(src.x), y(src.y), z(src.z) {}
+
+public:
+    TVector& operator +=(const TVector &v);
+    TVector& operator -=(const TVector &v);
+    TVector& operator *=(double s);
+    TVector& operator /=(double s);
+
+public:
+    double Abs() const;
+    double Abs2() const;
+    TVector Unit() const;
+    double Dot(const TVector &v) const ;
+    TVector Cross(const TVector &v) const ;
 
 };
 
 /**
- * Vector.
+ * Point.
  */
-typedef TCoordinate TVector;
+typedef TVector TCoordinate;
+
+/*
+ * Global operators.
+ */
+TVector operator +(const TVector &a, const TVector &b);
+TVector operator -(const TVector &a, const TVector &b);
+TVector operator *(const TVector &v, double s);
+TVector operator *(double s, const TVector &v);
+TVector operator /(const TVector &v, double s);
+TVector operator /(double s, const TVector &v);
 
 #endif
