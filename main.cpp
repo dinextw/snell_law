07fa2394696f94ede0125a6fe7147819ffe993f0
Ignatius <ice.emissary@gmail.com>
2016-10-21 07:44:54 +0800

    加入 Cuboid::ComputePath 的測試程式。

--- main.cpp
+++ main.cpp
@@ -11,5 +11,38 @@ int main(int argc, char *argv[])
                                  << mesh.CountCellsY() << ","
                                  << mesh.CountCellsZ() << ")" << endl;
 
+    // Cuboid path test.
+    Cuboid cell = mesh.GetCellByPoint(TVector(15, 30, 8));
+    {
+        cout << "Cuboid path calculate:" << endl;
+
+        TCoordinate source(10, 22, 6);
+        TVector direction(10, 16, 3);
+        cout << "  Source : (" << source.x << "," << source.y << "," << source.z << ")" << endl;
+        cout << "  Direction : (" << direction.x << "," << direction.y << "," << direction.z << ")" << endl;
+
+        auto result = cell.ComputePath(source, direction);
+
+        TCoordinate destination = result.first;
+        TVector plane = result.second;
+        cout << "  Destination : (" << destination.x << "," << destination.y << "," << destination.z << ")" << endl;
+        cout << "  Plane normal : (" << plane.x << "," << plane.y << "," << plane.z << ")" << endl;
+    }
+    {
+        cout << "Cuboid path calculate:" << endl;
+
+        TCoordinate source(20, 38, 9);
+        TVector direction(-10, -16, -3);
+        cout << "  Source : (" << source.x << "," << source.y << "," << source.z << ")" << endl;
+        cout << "  Direction : (" << direction.x << "," << direction.y << "," << direction.z << ")" << endl;
+
+        auto result = cell.ComputePath(source, direction);
+
+        TCoordinate destination = result.first;
+        TVector plane = result.second;
+        cout << "  Destination : (" << destination.x << "," << destination.y << "," << destination.z << ")" << endl;
+        cout << "  Plane normal : (" << plane.x << "," << plane.y << "," << plane.z << ")" << endl;
+    }
+
     return 0;
 }
