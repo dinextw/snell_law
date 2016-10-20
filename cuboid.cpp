15a784409e0336e82e2b270afb49c35ceeafda9e
Ignatius <ice.emissary@gmail.com>
2016-10-21 07:59:39 +0800

    微調 Cuboid::ComputePath 的版面。

--- cuboid.cpp
+++ cuboid.cpp
@@ -46,29 +46,72 @@ pair<TCoordinate, TVector> Cuboid::ComputePath(TCoordinate position_in, TVector 
     TCoordinate position_intersect_z_low = position_in + velocity_in * factor_multiply_z_low;
 
     // Compute Distance
+
     double distance_x_high = -1;
-    if (position_intersect_x_high.y <= y_high && position_intersect_x_high.y >= y_low && position_intersect_x_high.z <= z_high && position_intersect_x_high.z >= z_low)
-        distance_x_high = sqrt(pow(position_in.x - position_intersect_x_high.x, 2) + pow(position_in.y - position_intersect_x_high.y, 2) + pow(position_in.z - position_intersect_x_high.z, 2));
+    if( position_intersect_x_high.y <= y_high &&
+        position_intersect_x_high.y >= y_low &&
+        position_intersect_x_high.z <= z_high &&
+        position_intersect_x_high.z >= z_low )
+    {
+        distance_x_high = sqrt(  pow(position_in.x - position_intersect_x_high.x, 2)
+                               + pow(position_in.y - position_intersect_x_high.y, 2)
+                               + pow(position_in.z - position_intersect_x_high.z, 2) );
+    }
 
     double distance_x_low = -1;
-    if (position_intersect_x_low.y <= y_high && position_intersect_x_low.y >= y_low && position_intersect_x_low.z <= z_high && position_intersect_x_low.z >= z_low)
-        distance_x_low = sqrt(pow(position_in.x - position_intersect_x_low.x, 2) + pow(position_in.y - position_intersect_x_low.y, 2) + pow(position_in.z - position_intersect_x_low.z, 2));
+    if( position_intersect_x_low.y <= y_high &&
+        position_intersect_x_low.y >= y_low &&
+        position_intersect_x_low.z <= z_high &&
+        position_intersect_x_low.z >= z_low)
+    {
+        distance_x_low = sqrt(  pow(position_in.x - position_intersect_x_low.x, 2)
+                              + pow(position_in.y - position_intersect_x_low.y, 2)
+                              + pow(position_in.z - position_intersect_x_low.z, 2) );
+    }
 
     double distance_y_high = -1;
-    if (position_intersect_y_high.x >= x_low && position_intersect_y_high.x <= x_high && position_intersect_y_high.z >= z_low && position_intersect_y_high.z <= z_high)
-        distance_y_high = sqrt(pow(position_in.x - position_intersect_y_high.x, 2) + pow(position_in.y - position_intersect_y_high.y, 2) + pow(position_in.z - position_intersect_y_high.z, 2));
+    if( position_intersect_y_high.x >= x_low &&
+        position_intersect_y_high.x <= x_high &&
+        position_intersect_y_high.z >= z_low &&
+        position_intersect_y_high.z <= z_high)
+    {
+        distance_y_high = sqrt(  pow(position_in.x - position_intersect_y_high.x, 2)
+                               + pow(position_in.y - position_intersect_y_high.y, 2)
+                               + pow(position_in.z - position_intersect_y_high.z, 2) );
+    }
 
     double distance_y_low = -1;
-    if (position_intersect_y_low.x >= x_low && position_intersect_y_low.x <= x_high && position_intersect_y_low.z >= z_low && position_intersect_y_low.z <= z_high)
-        distance_y_low = sqrt(pow(position_in.x - position_intersect_y_low.x, 2) + pow(position_in.y - position_intersect_y_low.y, 2) + pow(position_in.z - position_intersect_y_low.z, 2));
+    if( position_intersect_y_low.x >= x_low &&
+        position_intersect_y_low.x <= x_high &&
+        position_intersect_y_low.z >= z_low &&
+        position_intersect_y_low.z <= z_high)
+    {
+        distance_y_low = sqrt(  pow(position_in.x - position_intersect_y_low.x, 2)
+                              + pow(position_in.y - position_intersect_y_low.y, 2)
+                              + pow(position_in.z - position_intersect_y_low.z, 2) );
+    }
 
     double distance_z_high = -1;
-    if (position_intersect_z_high.x >= x_low && position_intersect_z_high.x <= x_high && position_intersect_z_high.y >= y_low && position_intersect_z_high.y <= y_high)
-        distance_z_high = sqrt(pow(position_in.x - position_intersect_z_high.x, 2) + pow(position_in.y - position_intersect_z_high.y, 2) + pow(position_in.z - position_intersect_z_high.z, 2));
+    if( position_intersect_z_high.x >= x_low &&
+        position_intersect_z_high.x <= x_high &&
+        position_intersect_z_high.y >= y_low &&
+        position_intersect_z_high.y <= y_high)
+    {
+        distance_z_high = sqrt(  pow(position_in.x - position_intersect_z_high.x, 2)
+                               + pow(position_in.y - position_intersect_z_high.y, 2)
+                               + pow(position_in.z - position_intersect_z_high.z, 2) );
+    }
 
     double distance_z_low = -1;
-    if (position_intersect_z_low.x >= x_low && position_intersect_z_low.x <= x_high && position_intersect_z_low.y >= y_low && position_intersect_z_low.y <= y_high)
-        distance_z_low = sqrt(pow(position_in.x - position_intersect_z_low.x, 2) + pow(position_in.y - position_intersect_z_low.y, 2) + pow(position_in.z - position_intersect_z_low.z, 2));
+    if( position_intersect_z_low.x >= x_low &&
+        position_intersect_z_low.x <= x_high &&
+        position_intersect_z_low.y >= y_low &&
+        position_intersect_z_low.y <= y_high)
+    {
+        distance_z_low = sqrt(  pow(position_in.x - position_intersect_z_low.x, 2)
+                              + pow(position_in.y - position_intersect_z_low.y, 2)
+                              + pow(position_in.z - position_intersect_z_low.z, 2) );
+    }
 
     // Compare Distance
 
