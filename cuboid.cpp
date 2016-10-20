ee0522211e64891321ebe281ce300d7d36c24724
Ignatius <ice.emissary@gmail.com>
2016-10-20 23:54:21 +0800

    修改 Cuboid::ComputePath 的版面，將變數的宣告移至其第一次被使用處。

--- cuboid.cpp
+++ cuboid.cpp
@@ -29,52 +29,50 @@ void Cuboid::SetValue(double x_low,
 
 pair<TCoordinate, TVector> Cuboid::ComputePath(TCoordinate position_in, TVector velocity_in)
 {
-    TCoordinate position_intersect_x_high, position_intersect_x_low;
-    TCoordinate position_intersect_y_high, position_intersect_y_low;
-    TCoordinate position_intersect_z_high, position_intersect_z_low;
+    // Compute intersection position
 
-    double factor_multiply_x_high = 0, factor_multiply_x_low = 0;
-    double factor_multiply_y_high = 0, factor_multiply_y_low = 0;
-    double factor_multiply_z_high = 0, factor_multiply_z_low = 0;
+    double factor_multiply_x_high = abs((x_high - position_in.x) / velocity_in.x);
+    double factor_multiply_y_high = abs((y_high - position_in.y) / velocity_in.y);
+    double factor_multiply_z_high = abs((z_high - position_in.z) / velocity_in.z);
+    double factor_multiply_x_low = abs((x_low - position_in.x) / velocity_in.x);
+    double factor_multiply_y_low = abs((y_low - position_in.y) / velocity_in.y);
+    double factor_multiply_z_low = abs((z_low - position_in.z) / velocity_in.z);
 
-    double distance_x_high = -1, distance_x_low = -1;
-    double distance_y_high = -1, distance_y_low = -1;
-    double distance_z_high = -1, distance_z_low = -1;
+    TCoordinate position_intersect_x_high = position_in + velocity_in * factor_multiply_x_high;
+    TCoordinate position_intersect_y_high = position_in + velocity_in * factor_multiply_y_high;
+    TCoordinate position_intersect_z_high = position_in + velocity_in * factor_multiply_z_high;
+    TCoordinate position_intersect_x_low = position_in + velocity_in * factor_multiply_x_low;
+    TCoordinate position_intersect_y_low = position_in + velocity_in * factor_multiply_y_low;
+    TCoordinate position_intersect_z_low = position_in + velocity_in * factor_multiply_z_low;
 
-    TCoordinate position_out;
-    vector<double> distance(6);
-    vector<size_t> index_distance(6);
-
-
-    //Compute intersection position
-    factor_multiply_x_high = abs((x_high - position_in.x) / velocity_in.x);
-    factor_multiply_y_high = abs((y_high - position_in.y) / velocity_in.y);
-    factor_multiply_z_high = abs((z_high - position_in.z) / velocity_in.z);
-    factor_multiply_x_low = abs((x_low - position_in.x) / velocity_in.x);
-    factor_multiply_y_low = abs((y_low - position_in.y) / velocity_in.y);
-    factor_multiply_z_low = abs((z_low - position_in.z) / velocity_in.z);
-
-    position_intersect_x_high = position_in + velocity_in * factor_multiply_x_high;
-    position_intersect_y_high = position_in + velocity_in * factor_multiply_y_high;
-    position_intersect_z_high = position_in + velocity_in * factor_multiply_z_high;
-    position_intersect_x_low = position_in + velocity_in * factor_multiply_x_low;
-    position_intersect_y_low = position_in + velocity_in * factor_multiply_y_low;
-    position_intersect_z_low = position_in + velocity_in * factor_multiply_z_low;
-    //Compute Distance
+    // Compute Distance
+    double distance_x_high = -1;
     if (position_intersect_x_high.y <= y_high && position_intersect_x_high.y >= y_low && position_intersect_x_high.z <= z_high && position_intersect_x_high.z >= z_low)
         distance_x_high = sqrt(pow(position_in.x - position_intersect_x_high.x, 2) + pow(position_in.y - position_intersect_x_high.y, 2) + pow(position_in.z - position_intersect_x_high.z, 2));
+
+    double distance_x_low = -1;
     if (position_intersect_x_low.y <= y_high && position_intersect_x_low.y >= y_low && position_intersect_x_low.z <= z_high && position_intersect_x_low.z >= z_low)
         distance_x_low = sqrt(pow(position_in.x - position_intersect_x_low.x, 2) + pow(position_in.y - position_intersect_x_low.y, 2) + pow(position_in.z - position_intersect_x_low.z, 2));
+
+    double distance_y_high = -1;
     if (position_intersect_y_high.x >= x_low && position_intersect_y_high.x <= x_high && position_intersect_y_high.z >= z_low && position_intersect_y_high.z <= z_high)
         distance_y_high = sqrt(pow(position_in.x - position_intersect_y_high.x, 2) + pow(position_in.y - position_intersect_y_high.y, 2) + pow(position_in.z - position_intersect_y_high.z, 2));
+
+    double distance_y_low = -1;
     if (position_intersect_y_low.x >= x_low && position_intersect_y_low.x <= x_high && position_intersect_y_low.z >= z_low && position_intersect_y_low.z <= z_high)
         distance_y_low = sqrt(pow(position_in.x - position_intersect_y_low.x, 2) + pow(position_in.y - position_intersect_y_low.y, 2) + pow(position_in.z - position_intersect_y_low.z, 2));
+
+    double distance_z_high = -1;
     if (position_intersect_z_high.x >= x_low && position_intersect_z_high.x <= x_high && position_intersect_z_high.y >= y_low && position_intersect_z_high.y <= y_high)
         distance_z_high = sqrt(pow(position_in.x - position_intersect_z_high.x, 2) + pow(position_in.y - position_intersect_z_high.y, 2) + pow(position_in.z - position_intersect_z_high.z, 2));
+
+    double distance_z_low = -1;
     if (position_intersect_z_low.x >= x_low && position_intersect_z_low.x <= x_high && position_intersect_z_low.y >= y_low && position_intersect_z_low.y <= y_high)
         distance_z_low = sqrt(pow(position_in.x - position_intersect_z_low.x, 2) + pow(position_in.y - position_intersect_z_low.y, 2) + pow(position_in.z - position_intersect_z_low.z, 2));
 
-    //Compare Distance
+    // Compare Distance
+
+    vector<double> distance(6);
     distance[0] = distance_x_high;
     distance[1] = distance_x_low;
     distance[2] = distance_y_high;
@@ -82,8 +80,10 @@ pair<TCoordinate, TVector> Cuboid::ComputePath(TCoordinate position_in, TVector 
     distance[4] = distance_z_high;
     distance[5] = distance_z_low;
 
-    index_distance = SortIndexes(distance);
-    //Output Location
+    // Output Location
+
+    vector<size_t> index_distance = SortIndexes(distance);
+    TCoordinate position_out;
     switch (index_distance[5]) {
         case 0:
             position_out = position_intersect_x_high;
