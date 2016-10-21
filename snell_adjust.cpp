0f6e7f1c9ce7aa7b35d964515a651d994039a48e
Ignatius <ice.emissary@gmail.com>
2016-10-21 09:04:21 +0800

    調整 SnellAngleAdjust::ComputeRefractedVelocity 函式的版面，將變數宣告移至第一次使用時。

--- snell_adjust.cpp
+++ snell_adjust.cpp
@@ -1,6 +1,6 @@
 //
 //  snell_angle_adjust.cpp
-//  
+//
 //
 //  Created by 施俊宇 on 2016/10/19.
 //
@@ -8,25 +8,29 @@
 
 #include "snell_adjust.hpp"
 
-TVector SnellAngleAdjust::ComputeRefractedVelocity(TVector velocity_cell1, TVector norm_plane_vector, double speed_in, double speed_out)
+TVector SnellAngleAdjust::ComputeRefractedVelocity(TVector velocity_cell1,
+                                                   TVector norm_plane_vector,
+                                                   double  speed_in,
+                                                   double  speed_out)
 {
-    double r, c, coeff;
+    // Initialization
     velocity_in = velocity_cell1 / velocity_cell1.Abs();
     norm_plane = norm_plane_vector;
     speed_cell1 = speed_in;
     speed_cell2 = speed_out;
-    
-    //Initialization
-    r = speed_cell2/speed_cell1;
-    c = -norm_plane.Dot(velocity_in);
-    //Determine if Refraction or Total Reflection
+    double r = speed_cell2/speed_cell1;
+    double c = -norm_plane.Dot(velocity_in);
+
+    // Determine if Refraction or Total Reflection
     if (1 - pow(r, 2) * (1 - pow(c, 2)) >= 0)
-    {//Refraction
-        coeff = r * c - sqrt(1 - pow(r, 2) * (1 - pow(c, 2)));
+    {
+        // Refraction
+        double coeff = r * c - sqrt(1 - pow(r, 2) * (1 - pow(c, 2)));
         velocity_out = (velocity_in * r + norm_plane * coeff) * speed_cell2;
     }
     else
-    {//Total Reflection
+    {
+        // Total Reflection
         velocity_out = velocity_in;
         if (norm_plane.x != 0)
             velocity_out.x = -velocity_out.x;
@@ -35,7 +39,6 @@ TVector SnellAngleAdjust::ComputeRefractedVelocity(TVector velocity_cell1, TVect
         else
             velocity_out.z = -velocity_out.z;
     }
-    
-    
+
     return velocity_out;
 }
--- snell_adjust.hpp
+++ snell_adjust.hpp
@@ -1,6 +1,6 @@
 //
 //  snell_angle_adjust.hpp
-//  
+//
 //
 //  Created by 施俊宇 on 2016/10/19.
 //
@@ -17,10 +17,13 @@ private:
     TVector velocity_in, velocity_out;
     TVector norm_plane;
     double speed_cell1, speed_cell2;
-    
+
 public:
     SnellAngleAdjust() : speed_cell1(0), speed_cell2(0){};
-    TVector ComputeRefractedVelocity(TVector velocity_cell1, TVector norm_plane_vector, double speed_in, double speed_out);
+    TVector ComputeRefractedVelocity(TVector velocity_cell1,
+                                     TVector norm_plane_vector,
+                                     double  speed_in,
+                                     double  speed_out);
 };
 
 
