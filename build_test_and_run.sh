7e023164c410743aa6c4f46298f2d5c0b82a4007
Ignatius <ice.emissary@gmail.com>
2016-10-23 17:10:41 +0800

    修正測試程式執行檔名稱的錯誤。

--- build_test_and_run.sh
+++ build_test_and_run.sh
@@ -1,7 +1,7 @@
-g++ -o snell_law \
+g++ -o snell_law_test \
     snell_adjust.cpp \
     cuboid.cpp \
     mesh.cpp \
     path_tracker.cpp \
     snell_law_test.cpp \
-&& ./snell_law
+&& ./snell_law_test
